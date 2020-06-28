41cc8a9454c7ea7e92fedfe1b6fa80c431b6b1df
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-06-28 22:39:25 +0530

    adding server file for heroku

--- node_modules/ngx-i18nsupport/src/xliffmerge/main.ts
+++
@@ -1,7 +0,0 @@
-import {XliffMerge} from './xliff-merge';
-/**
- * Created by martin on 15.02.2017.
- * Command line interface xliffmerge
- */
-
-XliffMerge.main(process.argv);
---
+++ server.js
@@ -0,0 +1,12 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist/covid-dashboard'));
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/covid-dashboard/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
